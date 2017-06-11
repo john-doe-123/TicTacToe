febce4146bee0d5f199b830905ef5f88e3b8dac8
john-doe-123 <john.doe123@abv.bg>
2017-06-11 21:30:01 +0300

    Add printBanner, repeatChar.

--- src/HelperMethods.java
+++ src/HelperMethods.java
@@ -2,57 +2,16 @@ import java.util.Scanner;
 
 public class HelperMethods {
 
-    static void bannerPrint(String message, int width) {
-
-
-        int spaces = (width - message.length() - 2) / 2;
-
-
-        System.out.println(repeatChar('#', width));
-        System.out.printf("%1$s%2$s%1$s%n",
-                repeatChar(' ', spaces),
-                message.toUpperCase());
-        System.out.println(repeatChar('#', width));
-    }
-
     private static String repeatChar(char c, int numberOfTimes) {
         return new String(new char[numberOfTimes]).replace('\0', c);
     }
 
     static void printWelcomeMessage() {
 
-        bannerPrint("welcome!", 40);
+        printBanner("welcome!", 40);
         System.out.println();
     }
 
-//    static UserNumbers getUserInput(Board board) {
-//
-//        Scanner scanner = new Scanner(System.in);
-//
-//        int row = 0;
-//        int col = 0;
-//        boolean isOutsideRange = true;
-//
-//        do {
-//
-//            try {
-//                System.out.print("Row [1..3]: ");
-//                row = Integer.parseInt(scanner.nextLine());
-//                System.out.print("Column [1..3]: ");
-//                col = Integer.parseInt(scanner.nextLine());
-//
-//                isOutsideRange = (row < 1 || row > 3) || (col < 1 || col > 3);
-//
-//                row--;  // Subtract 1 because 1..3 is more user-friendly than 0..2;
-//                col--;
-//            } catch (NumberFormatException e) {
-//                System.out.println("Numbers only please!");
-//            }
-//
-//        } while (isOutsideRange || !board.isValidMove(board, row, col));
-//
-//        return new UserNumbers(row, col);
-//    }
 
     static UserNumbers getUserInput() {
 
@@ -67,22 +26,59 @@ public class HelperMethods {
             System.out.print("Column [1..3]: ");
             col = Integer.parseInt(scanner.nextLine());
         } catch (NumberFormatException e) {
-            System.out.println("Numbers only!");
+           // System.out.println("Numbers only!");
         }
 
-        return new UserNumbers(row, col);
+        return new UserNumbers(row - 1 , col - 1);
     }
 
-    static boolean isOutsideRange(UserNumbers userNumbers) {
+    private static boolean isOutsideRange(UserNumbers userNumbers) {
 
         boolean isOutsideRange;
 
-        int minRange = 1;
-        int maxRange = (Board.TOTAL_CELLS / 2) - 1; // -1 because the cells are numbered 1..9;
+        int minRange = 0;
+        int maxRange = Board.TOTAL_CELLS / 2;
 
         isOutsideRange = (userNumbers.row < minRange || userNumbers.row > maxRange) ||
                 (userNumbers.col < minRange || userNumbers.col > maxRange);
 
         return isOutsideRange;
+    }
+
+
+    static UserNumbers makeMove(Board board) {
+
+        UserNumbers userNumbers;
+
+        do {
+            userNumbers = getUserInput();
+
+            if (isOutsideRange(userNumbers) || !board.isValidMove(board, userNumbers)) {
+                System.out.println("Not a valid move or number. Please choose a free cell!");
+            }
+
+        } while (isOutsideRange(userNumbers) || !board.isValidMove(board, userNumbers));
+
+
+        return userNumbers;
+    }
+
+
+    static void printBanner(String message, int width) {
+
+        int leftSpaces = (width - message.length() - 2) / 2;
+        int rightSpaces = leftSpaces;
+
+        if (message.length() % 2 != 0 ^ width % 2 != 0) {
+            rightSpaces++;
+        }
+
+        System.out.println(repeatChar('#', width));
+        System.out.printf("#%1$s%2$s%3$s#%n",
+                repeatChar(' ', leftSpaces),
+                message.toUpperCase(),
+                repeatChar(' ', rightSpaces));
+        System.out.println(repeatChar('#', width));
+        System.out.println();
     }
 }
